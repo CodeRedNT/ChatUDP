44b6c2e8d42b746ee8d6a3d4671b2be874dfb482
root <root@localhost.localdomain>
2019-07-17 07:28:07 -0400

    fix client call's

--- venv/src/Cliente.py
+++ venv/src/Cliente.py
@@ -8,8 +8,8 @@ obj_socket.connect((servidor, porta))
 saida = ""
 
 while saida != "X":
-    msg = raw_input("Mensagem: ")
-    obj_socket.send(msg.encode(), porta)
+    msg = input("Mensagem: ")
+    obj_socket.sendto(msg.encode(), (servidor, porta))
     dados, origem = obj_socket.recvfrom(65535)
     print("Resposta do servidor: ", dados.decode())
     saida = input("X para fechar: ".upper())
